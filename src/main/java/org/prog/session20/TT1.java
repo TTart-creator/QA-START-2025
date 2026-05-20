4578493c95e804f6beb01c52e6a5e967e7972873
TTart-creator <t.kravchuk1989@gmail.com>
2026-05-20 11:26:18 +0200

    TT1, test

---
+++ src/main/java/org/prog/session20/TT1.java
@@ -0,0 +1,26 @@
+package org.prog.session20;
+
+public class TT1 {
+
+    public static void main(String[] args) {
+        int[] hotelRooms = new int[10];
+
+        hotelRooms[0] = 2;
+       hotelRooms[1] = 7;
+       hotelRooms[2] = 3;
+       hotelRooms[3] = 5;
+        hotelRooms[4] = 4;
+        hotelRooms[5] = 1;
+        hotelRooms[6] = 6;
+        hotelRooms[7] = 8;
+       hotelRooms[8] = 9;
+        hotelRooms[9] = 10;
+
+
+        for (int i=0; i < hotelRooms.length; i++)
+
+        System.out.println("Hotel Room " + i + " is " + hotelRooms[i]);
+
+    }
+
+}
